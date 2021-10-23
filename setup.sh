1e32eafb5de65694a3338274ac85c4e35b44649e
Bing <bing123456@gmail.com>
2021-10-24 02:20:58 +0800

    3333

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 ﻿image_version=`date +%Y%m%d%H%M`;
 echo $image_version;
-# cd Jenkins2Docker
+# cd mywebapp
 git pull --rebase origin master;
-docker stop jenkins2docker;
-docker rm jenkins2docker;
-docker build -t jenkins2docker:$image_version .;
+docker stop mywebapp;
+docker rm mywebapp;
+docker build -t mywebapp:$image_version .;
 docker images;
-docker run -p 10001:80 -d --name jenkins2docker jenkins2docker:$image_version;
+docker run -p 10001:80 -d --name mywebapp mywebapp:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-docker logs jenkins2docker;
+docker logs mywebapp;
 #删除build过程中产生的镜像    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
